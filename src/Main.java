596829c107dff72eda4d436ff294227369c6cd51
igm0313 <igm0313.handong.ac.kr>
2026-04-23 22:58:57 +0900

    make toString

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
-        ArrayList<Student> list;
+        ArrayList<Student> list = new ArrayList<Student>();
+        Student st = new Student("Lim",100);
+        list.add(st);
+        System.out.println(list.get(0).toString());
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,12 @@
 public class Student {
+    private String name;
+    private int score;
+    public Student(String name, int score){
+        this.name = name;
+        this.score= score;
+    }
+
+    public String toString(){
+        return name + " " + score;
+    }
 }
